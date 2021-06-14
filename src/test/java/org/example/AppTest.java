88bdda4d675aec67966a40f52341122ae20b9920
TASJA <tatyana.areshko@gmail.com>
2021-06-14 11:15:49 +0300

    commit for login Pinterest.com

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,31 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public static void main(String[] args) throws Exception {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        //открыть википедию
+        driver.get("https://www.wikipedia.org/");
+        //вписать в инпут "Селениум"
+        WebElement searchInput = driver.findElement(By.id("searchInput"));
+        searchInput.sendKeys("Selenium");
+        WebElement buttonSearch = driver.findElement(By.cssSelector("button.pure-button"));
+        buttonSearch.click();
+        WebElement header = driver.findElement(By.id("firstHeading"));
+        if (header.getText().equals("Selenium")) {
+            System.out.println("OK");
+        } else {
+            throw new Exception("No \"selenium\" in Header");
+        }
+        System.out.println("Breakpoint");
+        driver.quit();
     }
+
 }

0599738931e47fa52fa2b4e4bce23bf7aa99ccfc
TASJA <tatyana.areshko@gmail.com>
2021-06-25 13:23:15 +0300

    Parametr metod

--- src/main/java/com/pinterest/login/MainPage.java
+++ src/main/java/com/pinterest/login/MainPage.java
@@ -2,6 +2,7 @@ package com.pinterest.login;
 
 import io.qameta.allure.Step;
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -18,7 +19,7 @@ public class MainPage extends BasePage {
     @Step("Set sell amount: {value}")
     public void setValue(String value) {
         searchField.click();
-        searchField.clear();
+        searchField.sendKeys(Keys.HOME, Keys.chord(Keys.SHIFT, Keys.END), "");
         searchField.sendKeys(value);
         log.info("----------------------------------------VALUE----------------------------------------------");
     }
